add7f596fee0ec6bf3f5e0131d8577b4fd1f4c1f
Mahammed-Anish <122284524+Mahammed-Anish@users.noreply.github.com>
2023-07-21 12:04:15 +0530

    sort and merge two LL
    java program to sort and merge two Linked lists

---
+++ SortAndMergeLl.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class SortAndMergeLl {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n value: ");
+        int n = sc.nextInt();
+        LinkedList<Integer> ll1 = new LinkedList<>();
+        LinkedList<Integer> ll2 = new LinkedList<>();
+        LinkedList<Integer> ll3 = new LinkedList<>();
+
+        System.out.println("Enter elements into LinkedList1: ");
+        for(int i=0;i<n;i++) {
+            int val = sc.nextInt();
+            ll1.add(val);
+        }
+
+        System.out.println("Enter elements into LinkedList2: ");
+        for(int i=0;i<n;i++) {
+            int val = sc.nextInt();
+            ll2.add(val);
+        }
+
+        Collections.sort(ll1);
+        Collections.sort(ll2);
+
+        int i=0,j=0;
+        while(i<n && j<n) {
+            if(ll1.get(i) < ll2.get(j)) {
+                ll3.add(ll1.get(i));
+                i++;
+            } else if(ll1.get(i) > ll2.get(j)) {
+                ll3.add(ll2.get(j));
+                i++;
+            }
+        }
+
+        while(i<n) {
+            ll3.add(ll1.get(i));
+            i++;
+        }
+
+        while(j<n) {
+            ll3.add(ll2.get(j));
+            j++;
+        }
+
+        for(int k=0;k<ll3.size();k++) {
+            System.out.println("ll3: "+ll3.get(k));
+        }
+    }
+}
